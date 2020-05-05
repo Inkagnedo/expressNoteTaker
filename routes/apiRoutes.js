1c1a59186af177bf816e22a2b023359fdc04aa75
Inkagnedo <CCelestin44@gmail.com>
2020-05-05 19:33:51 -0400

    hope it works

---
+++ routes/apiRoutes.js
@@ -0,0 +1,42 @@
+var db = require("../db/db.json")
+var uniqid = require('uniqid')
+var fs = require('fs');
+var path = require('path')
+
+module.exports = function(app) {
+
+    app.get("/api/notes", function(req,res){
+        res.json(db)
+    });
+
+    app.post("/api/notes", function(req,res){
+        const id = uniqid()
+        const newNote = req.body;
+        newNote.id = id;
+        db.push(newNote);
+        res.json(db);
+        dbJ = JSON.stringify(db)
+        dbA = [dbJ]
+        fs.writeFile("./db/db.json", dbA, function(err){
+            if (err) throw err;
+            console.log("SAved")
+        })
+        
+    });
+
+    app.delete("/api/notes/:id", function(req,res){
+       const newNoteIndex = db.findIndex(function(note){
+        return note.id === req.params.id;   
+       });
+       const filtered = db.splice(newNoteIndex, 1);
+       res.json(filtered);
+       filteredJ = JSON.stringify(db)
+       filteredA = [filteredJ]
+       console.log(filteredA)
+       fs.writeFile("./db/db.json", filteredA, function(err){
+        if (err) throw err;
+        console.log("SAved")
+    })
+    })
+
+}
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-let express = require('express');
-let app = express();
-const PORT = process.env.PORT || 8080;
+var express = require("express");
 
-app.use(express.urlencoded({extended: true}));
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-app.listen(PORT, function() {
-  console.log("Listening on PORT: " + PORT);
-});
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+})
